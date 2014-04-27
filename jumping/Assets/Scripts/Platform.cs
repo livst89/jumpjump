760424c7e2877223e23c5cbc1ce014003e811512
livst89 <livst89@gmail.com>
2014-04-27 14:16:28 +0200

    Cleanup
    - Changed code to fit new asset names
    - Made changes to asset proportions and positions in scene
    - Environment object in Hierarchy to create order

--- jumping/Assets/Scripts/Platform.cs
+++ jumping/Assets/Scripts/Platform.cs
@@ -2,28 +2,13 @@
 using System.Collections;
 
 public class Platform : MonoBehaviour {
-
-	public Transform explosion;
+	
 	public int jumpSpeed = 7;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
 	void OnCollisionEnter2D(Collision2D other) {
-		//Debug.Log (other.gameObject.name);
-		if(other.gameObject.name == "kaj"){	
+		if(other.gameObject.name == "Player"){	
 			other.gameObject.rigidbody2D.velocity = Vector2.up * jumpSpeed;
 			Destroy (gameObject);
-			//print(other.gameObject.GetComponent<Moooove>().life--);
-		//Instantiate(explosion, transform.position, Quaternion.identity);
-		//Destroy(gameObject);
 		}
 	}
 }
--- jumping/Assets/Scripts/PlayerControl.cs
+++ jumping/Assets/Scripts/PlayerControl.cs
@@ -7,17 +7,9 @@ public class PlayerControl : MonoBehaviour
 		public Transform speed;
 
 	public int jumpSpeed;
-		// Use this for initialization
-		void Start ()
-		{
-	
-		}
-	
-		// Update is called once per frame
+
 		void FixedUpdate ()
 		{
-
-				//		transform.position += new Vector2(0, 0.5f);
 
 				if (Input.GetKey (KeyCode.D)) {
 						transform.Translate (Vector2.right * 4f * Time.deltaTime);
@@ -28,20 +20,9 @@ public class PlayerControl : MonoBehaviour
 				}
 				if (Input.GetKey (KeyCode.A)) {
 						transform.Translate (-Vector2.right * 4f * Time.deltaTime);
-						// transform.eulerAngles = new Vector2(0, 180); // This one is evil, and dosenøt work, becuase it is euler?
 				}	
 				if (Input.GetKey (KeyCode.W)) {
 						transform.Translate (Vector2.up * 8f * Time.deltaTime);
 				}
 		}
-
-//		void OnCollisionEnter2D (Collision2D other)
-//		{
-//
-//				if (other.gameObject.name == "Platform") {
-//						print ("Collided with platform");
-//			rigidbody2D.velocity =  Vector2.up * jumpSpeed;
-//						Destroy(other.gameObject);
-//				}
-//		}
 }
