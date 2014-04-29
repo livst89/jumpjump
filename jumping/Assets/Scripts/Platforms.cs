2027010cc2aa455be106cb513a1a7e1f7beede98
livst89 <livst89@gmail.com>
2014-04-29 15:06:06 +0200

    Revert "Revert 4b74f8a..20652c4"
    This reverts commit de39bc4d23741d05e5d6eeb38e9fffb0d497ca57.

--- jumping/Assets/Scripts/Platforms.cs
+++ jumping/Assets/Scripts/Platforms.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Platforms : MonoBehaviour {
 
-	public GameController gameController; // Object for referring to the GameController script
-	
-	public int jumpSpeed = 7;
+	private GameController gameController; // Object for referring to the GameController script
+	private ScoreKeeper scoreKeeper; // Object for referring to the GameController script
+
+	public float speed;
+	public float jumpForce;		// Value must be set in the Platform prefab Inspector.
+
+	public AudioClip Jump1;		// Jump sound clip
 
 	void Start () {
 		// Check to ensure that a GameController object is in the scene
@@ -18,13 +22,35 @@ public class Platforms : MonoBehaviour {
 		{
 			Debug.Log ("Cannot find 'GameController' script");
 		}
+
+		GameObject scoreKeeperObject = GameObject.FindWithTag ("ScoreKeeper");
+		if (scoreKeeperObject != null)
+		{
+			scoreKeeper = scoreKeeperObject.GetComponent <ScoreKeeper>();
+		}
+		if (scoreKeeper == null)
+		{
+			Debug.Log ("Cannot find 'Score' script");
+		}
+
+		rigidbody2D.velocity = transform.up * speed;
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
 		if(other.gameObject.name == "Player"){
-			gameController.AddPoint();
-			other.gameObject.rigidbody2D.velocity = Vector2.up * jumpSpeed;
-			Destroy (gameObject);
+			AudioSource.PlayClipAtPoint(Jump1, transform.position);		// Play jumping sound
+			scoreKeeper.AddPoint();		// Add points to score in GameController
+			other.gameObject.rigidbody2D.AddForce(new Vector2(0.0f,jumpForce));		// Push the Player upwards like a jump
+			gameObject.SetActive(false); // Set the platform as inactive in the hierarchy
 		}
+
+		// Buggy, fix later!
+		else if (other.gameObject.name == "Ground") {
+			Debug.Log ("Platform hit ground");
+			gameObject.SetActive(false); // Set the platform as inactive in the hierarchy
+
+		}
+
+
 	}
 }
